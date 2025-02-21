f3133911d9868d672fae49faa1ecad98088bf3bf
David Maimon <maimonides3691@gmail.com>
2025-02-21 00:40:44 -0500

    2

--- app/aws-config.ts
+++ app/aws-config.ts
@@ -12,14 +12,6 @@ const config = {
     region: process.env.NEXT_PUBLIC_AWS_REGION!,
     userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID!,
     userPoolWebClientId: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID!,
-    mandatorySignIn: false,
-    oauth: {
-      domain: 'alumintel.auth.us-east-2.amazoncognito.com',
-      scope: ['email', 'openid', 'profile'],
-      redirectSignIn: 'https://www.alumintel.com/dashboard',
-      redirectSignOut: 'https://www.alumintel.com/',
-      responseType: 'code',
-    },
   },
 };
 
