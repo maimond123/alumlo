70b8046673511487a15456a6506cd2971732135f
David Maimon <maimonides3691@gmail.com>
2025-02-21 00:37:42 -0500

    1

--- app/aws-config.ts
+++ app/aws-config.ts
@@ -9,28 +9,19 @@ if (
 
 const config = {
   Auth: {
-    // REQUIRED - Amazon Cognito Region
     region: process.env.NEXT_PUBLIC_AWS_REGION!,
-    // OPTIONAL - Amazon Cognito Identity Pool ID (if using federated identities)
-    identityPoolId: process.env.NEXT_PUBLIC_IDENTITY_POOL_ID!,
-    // REQUIRED - Amazon Cognito User Pool ID
     userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID!,
-    // REQUIRED - Amazon Cognito Web Client ID (note the key name change)
     userPoolWebClientId: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID!,
-    // OPTIONAL - Enforce user authentication prior to accessing AWS resources
     mandatorySignIn: false,
-    // OPTIONAL - Hosted UI configuration
     oauth: {
       domain: 'alumintel.auth.us-east-2.amazoncognito.com',
       scope: ['email', 'openid', 'profile'],
-      // Use fully-qualified URLs (including protocol)
       redirectSignIn: 'https://www.alumintel.com/dashboard',
       redirectSignOut: 'https://www.alumintel.com/',
       responseType: 'code',
     },
   },
 };
-
 
 console.log('Final AWS Config:', config);
 Amplify.configure(config as any);
