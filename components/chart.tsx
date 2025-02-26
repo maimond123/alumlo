01afeb9a4115bbbd9bc61d6232ce8219e6a1d40e
David Maimon <maimonides3691@gmail.com>
2025-02-26 13:10:58 -0500

    p

--- components/chart.tsx
+++ components/chart.tsx
@@ -98,7 +98,7 @@ export function BarChart({ data, isZoomed = false }: ChartProps) {
           <Tooltip content={<ChartTooltipContent />} />
           <Bar 
             dataKey="value" 
-            fill="#2d6a4f80" 
+            fill="#00C49F80" 
             radius={[4, 4, 0, 0]} 
             isAnimationActive={isZoomed} 
           />
