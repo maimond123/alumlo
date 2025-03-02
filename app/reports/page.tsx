99a97d116e73283df2b72dc6a19e9801ebc0a4aa
David Maimon <maimonides3691@gmail.com>
2025-03-01 21:48:08 -0500

    report 3

--- app/reports/page.tsx
+++ app/reports/page.tsx
@@ -542,12 +542,6 @@ function ReportsContent() {
               exit={{ scale: 0.9 }}
               className="bg-gray-700 p-8 max-w-4xl w-full max-h-[90vh] overflow-auto relative rounded-lg"
             >
-              <button
-                onClick={() => setIsExpanded(false)}
-                className="absolute top-4 right-4 text-gray-500 hover:text-gray-700"
-              >
-                <X className="w-6 h-6" />
-              </button>
               <ReportContent />
               <div className="mt-4 flex justify-center space-x-4">
                 <button
