35861dc36b403766f44d15be0d6c9eb4f489e897
David Maimon <maimonides3691@gmail.com>
2025-03-05 16:51:16 -0500

    major change

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,15 @@ module.exports = {
         '80': '0.80',
         '105': '1.05',
         '107': '1.07',
+      },
+      keyframes: {
+        fadeIn: {
+          'from': { opacity: 0, transform: 'translateY(10px)' },
+          'to': { opacity: 1, transform: 'translateY(0)' }
+        }
+      },
+      animation: {
+        'fadeIn': 'fadeIn 0.5s ease-out forwards'
       }
     },
   },
