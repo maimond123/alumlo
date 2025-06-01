b77b14bdcc1244e65ac96c32aec2ce955846d0e4
David Maimon <maimonides3691@gmail.com>
2025-06-01 15:53:55 -0400

    1

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,13 @@ import FeaturesSection from '../components/FeaturesSection'
 import Footer from '../components/footer'
 
 export default function Home() {
-  // Approximate height of the navigation bar in its scaled state (if it were scaled)
-  // Or, more simply, the unscaled height if the main content padding is inside the scaled area.
-  // Let's assume the nav bar is roughly 68px high. If scaled by 0.82, this is 68 / 0.82 = ~83px
-  // This padding will be applied *inside* the .scaled-content-area
-  const navHeightForPadding = 'pt-[83px]'; // Adjust this value based on actual nav height
-
   return (
     <>
       <Navigation /> 
-      <div className="scaled-content-wrapper">
-        <div className={`scaled-content-area ${navHeightForPadding}`}>
-          <Hero />
-          <FeaturesSection />
-          <Footer />
-        </div>
+      <div className="pt-20"> {/* Standard padding for navigation */}
+        <Hero />
+        <FeaturesSection />
+        <Footer />
       </div>
     </>
   )
